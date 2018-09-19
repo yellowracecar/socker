080d2a214906cf7e882529b2569d890c843c35f8
Gustav Laestander <lagu16@nti.umea>
2018-09-19 13:28:59 +0200

    sockerprogramEZmode

---
+++ Sockerantal.py
@@ -0,0 +1,8 @@
+print("Hej och välkommen till socker programmet")
+sockerbit = 3.2
+
+produkt = str(input("Vad har du ätit idag? "))
+socker = int(input("Hur många gram socker innehåller det?" ))
+
+print("Antal socker bitar i", produkt)
+print(socker / sockerbit)
